e1d631ee93dc9a56b7120a13d9a2d1fc4d7e4e89
Milad Heydari <miladheydari.work@gmail.com>
2018-10-08 18:36:02 +0330

    devtool

--- next.config.js
+++ next.config.js
@@ -2,68 +2,70 @@ const path = require("path");
 const glob = require("glob");
 
 module.exports = {
-  exportPathMap: async function(defaultPathMap) {
-    return {
-      "/": { page: "/" }
-    };
-  },
+    exportPathMap: async function(defaultPathMap) {
+        return {
+            "/": { page: "/" }
+        };
+    },
 
-  webpack: (config, env) => {
-    config.resolve.alias = {
-      components: path.resolve(__dirname, "./components"),
-      helpers: path.resolve(__dirname, "./helpers"),
-      languageProvider: path.resolve(__dirname, "./languageProvider"),
-      image: path.resolve(__dirname, "./static/image"),
-      pages: path.resolve(__dirname, "./pages"),
-      config: path.resolve(__dirname, "./config")
-    };
-    config.module.rules.push(
-      {
-        test: /\.(css|scss|less)/,
-        loader: "emit-file-loader",
-        options: {
-          name: "dist/[path][name].[ext]"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ["babel-loader", "raw-loader", "postcss-loader"]
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: [
-          "babel-loader",
-          "raw-loader",
-          "postcss-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              includePaths: ["styles", "node_modules"]
-                .map(d => path.join(__dirname, d))
-                .map(g => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
+    webpack: (config, env) => {
+        if(env.dev === false)
+            config.devtool = false;
+        config.resolve.alias = {
+            components: path.resolve(__dirname, "./components"),
+            helpers: path.resolve(__dirname, "./helpers"),
+            languageProvider: path.resolve(__dirname, "./languageProvider"),
+            image: path.resolve(__dirname, "./static/image"),
+            pages: path.resolve(__dirname, "./pages"),
+            config: path.resolve(__dirname, "./config")
+        };
+        config.module.rules.push(
+            {
+                test: /\.(css|scss|less)/,
+                loader: "emit-file-loader",
+                options: {
+                    name: "dist/[path][name].[ext]"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["babel-loader", "raw-loader", "postcss-loader"]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    "babel-loader",
+                    "raw-loader",
+                    "postcss-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: ["styles", "node_modules"]
+                                .map(d => path.join(__dirname, d))
+                                .map(g => glob.sync(g))
+                                .reduce((a, c) => a.concat(c), [])
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    "babel-loader",
+                    "raw-loader",
+                    "postcss-loader",
+                    {
+                        loader: "less-loader",
+                        options: {
+                            includePaths: ["styles", "node_modules"]
+                                .map(d => path.join(__dirname, d))
+                                .map(g => glob.sync(g))
+                                .reduce((a, c) => a.concat(c), [])
+                        }
+                    }
+                ]
             }
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          "babel-loader",
-          "raw-loader",
-          "postcss-loader",
-          {
-            loader: "less-loader",
-            options: {
-              includePaths: ["styles", "node_modules"]
-                .map(d => path.join(__dirname, d))
-                .map(g => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
-          }
-        ]
-      }
-    );
-    return config;
-  }
+        );
+        return config;
+    }
 };
